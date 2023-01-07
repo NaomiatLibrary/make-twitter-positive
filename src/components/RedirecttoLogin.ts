42377bebdc8d5cc043a25f3824844534cdac0bac
usaneko-xlarge <naomi.lilienthal.jpn@gmail.com>
2023-01-08 02:21:48 +0900

    parse response update

--- src/components/RedirecttoLogin.ts
+++ src/components/RedirecttoLogin.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const RedirecttoLogin = () =>{
     axios.get(' https://t2zuzepsh3.execute-api.ap-northeast-1.amazonaws.com/Prod/hello/')
   .then(response=>{
-    const data=JSON.parse(response.data)
-    document.location = data.url;
+    console.log(response)
+    document.location = response.data.url;
   })
 };
