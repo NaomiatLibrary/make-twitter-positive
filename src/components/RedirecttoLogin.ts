391112b8403462a86beae72b9fa7a276b905fb51
usaneko-xlarge <naomi.lilienthal.jpn@gmail.com>
2023-01-08 02:02:36 +0900

    apiurl update

--- src/components/RedirecttoLogin.ts
+++ src/components/RedirecttoLogin.ts
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from "axios";
 export const RedirecttoLogin = () =>{
-    axios.get('https://liyrpmqvlf.execute-api.ap-northeast-1.amazonaws.com/Prod/hello/')
+    axios.get(' https://t2zuzepsh3.execute-api.ap-northeast-1.amazonaws.com/Prod/hello/')
   .then(response=>{
     const data=JSON.parse(response.data)
     document.location = data.url;
